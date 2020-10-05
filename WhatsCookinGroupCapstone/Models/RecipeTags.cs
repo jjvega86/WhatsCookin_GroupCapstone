b1cedb038d3072b3945ae75b75b8f657eaeac247
NicholasthedevJacobs <nicholas.jjacobs@gmail.com>
2020-10-05 15:54:58 -0500

    Added properties to the RecipeTags and Followers classes.

--- WhatsCookinGroupCapstone/Models/RecipeTags.cs
+++ WhatsCookinGroupCapstone/Models/RecipeTags.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace WhatsCookinGroupCapstone.Models
 {
     public class RecipeTags
     {
+        [ForeignKey ("Recipe")]
+        public int RecipeID { get; set; }
+        public Recipe Recipe { get; set; }
+
+        [ForeignKey ("Tags")]
+        public int TagID { get; set; }
+        public Tags Tags { get; set; }
     }
 }
