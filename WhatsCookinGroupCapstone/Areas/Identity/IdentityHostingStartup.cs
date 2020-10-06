5423c108edeebac645d0512559cd9f9b39fc0aa1
NicholasthedevJacobs <nicholas.jjacobs@gmail.com>
2020-10-06 14:40:15 -0500

    Edited the register.cshtml class to work with mvc identity.

---
+++ WhatsCookinGroupCapstone/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WhatsCookinGroupCapstone.Data;
+
+[assembly: HostingStartup(typeof(WhatsCookinGroupCapstone.Areas.Identity.IdentityHostingStartup))]
+namespace WhatsCookinGroupCapstone.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
