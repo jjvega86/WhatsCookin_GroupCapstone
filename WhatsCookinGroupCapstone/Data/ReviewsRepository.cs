353afa1deaefd82588187ce491bce925c67719aa
jjvega86 <jj.vega86@gmail.com>
2020-10-06 14:53:45 -0400

    Fixed using statement errors in multiple repositories

--- WhatsCookinGroupCapstone/Data/ReviewsRepository.cs
+++ WhatsCookinGroupCapstone/Data/ReviewsRepository.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WhatsCookinGroupCapstone.Contracts;
 using WhatsCookinGroupCapstone.Models;
 using static WhatsCookinGroupCapstone.Contracts.IRepositoryBase;
 
 namespace WhatsCookinGroupCapstone.Data
 {
-    //public class ReviewsRepository : RepositoryBase<Reviews>, IReviewsRepository
-    //{
-    //    public ReviewsRepository(ApplicationDbContext applicationDbContext)
-    //        : base(applicationDbContext)
-    //    {
-    //    }
+    public class ReviewsRepository : RepositoryBase<Reviews>, IReviewsRepository
+    {
+        public ReviewsRepository(ApplicationDbContext applicationDbContext)
+            : base(applicationDbContext)
+        {
+        }
 
-    //}
+    }
 }
