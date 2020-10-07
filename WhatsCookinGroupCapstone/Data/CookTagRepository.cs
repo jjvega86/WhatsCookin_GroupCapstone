fc22d01dce66f56d09e424bd2e00cac8d1630e47
NicholasthedevJacobs <nicholas.jjacobs@gmail.com>
2020-10-07 09:53:04 -0500

    Created appropriate classes and interfaces for CookTag.

---
+++ WhatsCookinGroupCapstone/Data/CookTagRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatsCookinGroupCapstone.Contracts;
+using WhatsCookinGroupCapstone.Models;
+
+namespace WhatsCookinGroupCapstone.Data
+{
+    public class CookTagRepository : RepositoryBase<CookTag>, ICookTagRepository
+    {
+        public CookTagRepository(ApplicationDbContext applicationDbContext)
+            : base(applicationDbContext)
+        {
+        }
+    }
+   
+}
