dfff168258f243f59836107285cbfd2cbd214826
ktenbrook1 <katelynntenbrook@gmail.com>
2020-10-08 08:45:31 -0500

    Added hidden API keys

---
+++ WhatsCookinGroupCapstone/Models/APIKey/Key.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatsCookinGroupCapstone.Models.APIKey
+{
+    public static class Key
+    {
+        public static string clientId;
+        public static string clientSecret;
+            static Key()
+        {
+            clientId = "164158722967 - kklsl8kbp4mu212ldp0bmsm2b01d60c9.apps.googleusercontent.com";
+            clientSecret = "SRkHlB1E9P2zJIam37_wINg6";
+        }
+    }
+}
