4b4d47ebc9141dd3cd4d9b0b03a9ad5e9db0bf25
jjvega86 <jj.vega86@gmail.com>
2020-10-08 15:07:26 -0400

    Tested Review creation funcionality

--- WhatsCookinGroupCapstone/Models/Reviews.cs
+++ WhatsCookinGroupCapstone/Models/Reviews.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,7 +10,8 @@ namespace WhatsCookinGroupCapstone.Models
     public class Reviews
     {
         //All subject to change depending on the API we will be adding later
-        public int Id { get; set; }
+        [Key]
+        public int ReviewId { get; set; }
         public int Rating { get; set; }
         public string ReviewForRecipe { get; set; }
         public bool Validation { get; set; }
