249c6766cf8e42e431cf63b62244360d676c32eb
NicholasthedevJacobs <nicholas.jjacobs@gmail.com>
2020-10-08 20:02:47 -0500

    Figured out how to get the imageurl into the index view from a property in the model.

--- WhatsCookinGroupCapstone/Data/ApplicationDbContext.cs
+++ WhatsCookinGroupCapstone/Data/ApplicationDbContext.cs
@@ -28,67 +28,7 @@ namespace WhatsCookinGroupCapstone.Data
            }
            );
 
-           // builder.Entity<Recipe>()
-           //.HasData(
-           //new Recipe
-           //{
-           //    RecipeId = 1,
-           //    RecipeName = "Beef Stroganoff",
-           //    IMGUrl = "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F2394312.jpg",
-           //    Ingredients = "Ingredients",
-           //    Description = "Description",
-           //    Steps = "Steps",
-           //    CookID = null
-           //},
-           //new Recipe
-           //{
-           //    RecipeId = 2,
-           //    RecipeName = "Zucchini Artichoke Summer Salad",
-           //    IMGUrl = "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F1131616.jpg&w=596&h=399&c=sc&poi=face&q=85",
-           //    Ingredients = "Ingredients",
-           //    Description = "Description",
-           //    Steps = "Steps"
-           //},
-           //new Recipe
-           //{
-           //    RecipeId = 3,
-           //    RecipeName = "Jorge's Pasta-less Eggplant Lasagna",
-           //    IMGUrl = "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F863823.jpg&w=596&h=596&c=sc&poi=face&q=85",
-           //    Ingredients = "Ingredients",
-           //    Description = "Description",
-           //    Steps = "Steps"
-           //},
-           //new Recipe
-           //{
-           //    RecipeId = 4,
-           //    RecipeName = "Thai Peanut Stir Fry",
-           //    IMGUrl = "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F4485705.jpg&w=596&h=792&c=sc&poi=face&q=85",
-           //    Ingredients = "Ingredients",
-           //    Description = "Description",
-           //    Steps = "Steps"
-           //},
-           //new Recipe
-           //{
-           //    RecipeId = 5,
-           //    RecipeName = "Shrimp Red Thai Curry",
-           //    IMGUrl = "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F6445095.jpg&q=85",
-           //    Ingredients = "Ingredients",
-           //    Description = "Description",
-           //    Steps = "Steps"
-           //},
-           //new Recipe
-           //{
-           //    RecipeId = 6,
-           //    RecipeName = "Carrot, Tomato, and Spinach Quinoa Pilaf with Ground Turkey",
-           //    IMGUrl = "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F2281755.jpg",
-           //    Ingredients = "Ingredients",
-           //    Description = "Description",
-           //    Steps = "Steps"
-           //}
-
-
-
-           //);
+           
 
             builder.Entity<Tags>().HasData(
                 new Models.Tags
